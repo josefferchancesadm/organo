e27fb2ad806607b77cb94de888eed08dac17f56f
josefferchancesadm <josefferchancesadm@example.com>
2024-03-18 19:24:56 -0300

    ok

---
+++ src/componentes/ListaSuspensa/Index.js
@@ -0,0 +1,24 @@
+import "./ListaSuspensa.css";
+
+const ListaSuspensa = (props) => {
+  console.log(props);
+  return (
+    <div className="lista-suspensa">
+      <label>{props.label}</label>
+
+      <select
+        onChange={(evento) => props.aoAlterado(evento.target.value)}
+        required={props.obrigatorio}
+        value={props.valor}
+      >
+        <option value=""></option>
+
+        {props.itens.map((item) => {
+          return <option key={item.nome}>{item.nome}</option>;
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default ListaSuspensa;
